3caab2ee5c15dfdedebc68498608e8c7f583371d
jpuptagrafft <46825889+jpuptagrafft@users.noreply.github.com>
2024-02-05 12:26:04 -0800

    feb5

---
+++ hello_world.go
@@ -0,0 +1,8 @@
+package main 
+
+import "fmt"
+//Packages and imports work how you would think; Just grabbing things from libraries
+
+func main(){
+	fmt.Println("Hello World")
+}
---
+++ messing_with_numbers.go
@@ -0,0 +1,13 @@
+package main 
+
+import "fmt"
+
+
+func main(){
+	fmt.Println("7/3 = ", 7/3)
+	fmt.Println("7.0/3.0 = ", 7.0/3.0)
+	fmt.Println("7.0/3 = ", 7.0/3)
+	//If you divide two ints together, it will automatically do int division
+	//any float division will result in normal division
+	//Remember, Comma to Concatinate with non strings!
+}
